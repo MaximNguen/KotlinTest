8652a4b26eebb6b76c94915b0cdd54bf203d8613
MaximNguen <foditoon1@gmail.com>
2023-02-11 23:15:22 +0400

    GameDungeon
    Немного узнал про функцию области видимости? (не факт, что эта та самая функция)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,37 +1,36 @@
-fun main(args: Array<String>) {
+data class Datas(val name: String, var currentMoney: Int, var healthPoints: Int, var heals: Int)
+
+fun main() {
     println("Введите свое имя в качестве игрока")
-    val name = readLine()
-    var healthPoints = 100
-    var heals = 2
-    var currentMoney :Int = 0
+    val player = Datas("Max", 0, 100, 2)
     val enemies = listOf(1,2,3)
 
-    println("Вас зовут $name. У вас 100 здоровья... \nУ вас есть зелья, которые лечал вас по 40 здоровья \nВы решили пойти в подземелье, где есть 10 уровней, смотря на вашу удачи и тактику, посмотрим сможете ли вы выжить... \nНа каждом этаже есть 3 прохода, в одной из них затаился враг, способный нанести вам 35 здоровья... Будьте осторожны с выбором прохода...")
+    println("Вас зовут ${player.name}. У вас 100 здоровья... \nУ вас есть зелья, которые лечал вас по 40 здоровья \nВы решили пойти в подземелье, где есть 10 уровней, смотря на вашу удачи и тактику, посмотрим сможете ли вы выжить... \nНа каждом этаже есть 3 прохода, в одной из них затаился враг, способный нанести вам 35 здоровья... Будьте осторожны с выбором прохода...")
 
     for (i in 1..10) {
         println("\nВы вошли в $i уровень... Видите перед с собой 3 прохода к следующему уровню, выберете один из 3...")
         val playerPosition :Int = readLine()!!.toInt()
         val enemyPosition = enemies.random()
         if (playerPosition == enemyPosition) {
-            healthPoints -= 35
-            println("О нет! Это проход был с врагом! Ваше здоровье составляет - $healthPoints")
+            player.healthPoints -= 35
+            println("О нет! Это проход был с врагом! Ваше здоровье составляет - ${player.healthPoints}")
         }
-        if (heals == 0) {
+        if (player.heals == 0) {
             println("У ВАС ЗАКОНЧИЛИСЬ ЗЕЛЬЯ ЛЕЧЕНИЯ!")
         }
-        while (healthPoints < 100 && healthPoints > 0 && heals > 0) {
+        while (player.healthPoints < 100 && player.healthPoints > 0 && player.heals > 0) {
             println("У вас есть зелья! Вы хотите их использовать? Yes or No?")
             val answer = readLine()
             if (answer == "Yes") {
-                if (100 - healthPoints > 40) {
-                    healthPoints += 40
-                    println("Вы восстановили здоровье до $healthPoints")
-                    heals -= 1
+                if (100 - player.healthPoints > 40) {
+                    player.healthPoints += 40
+                    println("Вы восстановили здоровье до ${player.healthPoints}")
+                    player.heals -= 1
                     break
                 } else {
-                    healthPoints = 100
+                    player.healthPoints = 100
                     println("Вы восстановили здоровье полностью")
-                    heals -= 1
+                    player.heals -= 1
                     break
                 }
             }
@@ -39,33 +38,33 @@ fun main(args: Array<String>) {
                 break
             }
         }
-        if (healthPoints > 1 && playerPosition == enemyPosition) {
-            currentMoney += 50
+        if (player.healthPoints > 1 && playerPosition == enemyPosition) {
+            player.currentMoney += 50
             println("За очистку уровня с врагом вы получаете 50 монет!")
-        }  else if (healthPoints > 1 && playerPosition != enemyPosition) {
+        }  else if (player.healthPoints > 1 && playerPosition != enemyPosition) {
             println("За безопасную очистку вы получаете 30 монет!")
-            currentMoney += 30
+            player.currentMoney += 30
         }
 
-        if (healthPoints < 1) {
+        if (player.healthPoints < 1) {
             println("\nВас убили! Вы дошли до $i уровня!")
-            println("У вас $currentMoney монет!")
+            println("У вас ${player.currentMoney} монет!")
             println("Ваше здоровье - 0")
             break
         }
-        if (healthPoints > 1 && i == 10) {
+        if (player.healthPoints > 1 && i == 10) {
             println("\nВы дошли до $i уровня! Вы прошли игру!")
-            println("У вас $currentMoney монет!")
-            println("Ваше здоровье - $healthPoints")
+            println("У вас ${player.currentMoney} монет!")
+            println("Ваше здоровье - ${player.healthPoints}")
             break
         }
-        if (healthPoints < 1 && i == 10) {
+        if (player.healthPoints < 1 && i == 10) {
             println("\nВы дошли до $i уровня! Вы почти прошли игру!")
-            println("У вас $currentMoney монет!")
+            println("У вас ${player.currentMoney} монет!")
             println("Ваше здоровье - 0")
             break
         }
-        println("Ваше здоровье - $healthPoints")
-        println("Монеты - $currentMoney")
+        println("Ваше здоровье - ${player.healthPoints}")
+        println("Монеты - ${player.currentMoney}")
     }
 }
